a5fc862ebc2e5931c2aa8a8e3d5e56cf54009666
mcgarcia <mcgarcia@novopayment.com>
2021-04-14 16:32:44 -0500

    first commit

---
+++ src/test/java/com/platzi/fundamentosplatzi/FundamentosPlatziApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platzi.fundamentosplatzi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosPlatziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
